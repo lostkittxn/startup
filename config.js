2abf9458f57a972869cceaf565039b1768bedbe5
lostkittxn <126134611+lostkittxn@users.noreply.github.com>
2023-02-22 22:01:34 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'Layla',
+	imageBackground: false,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Buenos Dias!',
+	greetingAfternoon: 'Buenas Tardes,',
+	greetingEvening: 'Buenas Noches,',
+	greetingNight: 'Hora de Dormir!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '37.775',
+	defaultLongitude: '-122.419',
+
+	// Autochange
+	autoChangeTheme: true,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'music',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://outlook.live.com/owa/',
+		},
+		{
+			id: '3',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '5',
+			name: 'Canva',
+			icon: 'pen-tool',
+			link: 'https://www.canva.com/',
+		},
+		{
+			id: '6',
+			name: 'YouTube',
+			icon: 'youtube',
+			link: 'https://www.youtube.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'music',
+			id: '1',
+			links: [
+				{
+					name: 'Femcel Core',
+					link: 'https://open.spotify.com/playlist/2YukkVshmXz1WKeqoSQTDD?si=e75c9fb8aa8541f4',
+				},
+				{
+					name: 'OMAM',
+					link: 'https://open.spotify.com/playlist/5paYOcauXYoUD4igOyhKkY?si=05156a754cf5491e',
+				},
+				{
+					name: 'Rolitas',
+					link: 'https://open.spotify.com/playlist/09DxIIDphVuj5L9gTCVrjq?si=6daa607f2b534fe6Q',
+				},
+				{
+					name: 'Favoritas',
+					link: 'https://open.spotify.com/playlist/0kzaVFVUIaQ7QWnd6c1AfA?si=84d1393c87a84637',
+				},
+			],
+		},
+		{
+			icon: 'coffee',
+			id: '2',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Pinterest',
+					link: 'https://www.pinterest.com',
+				},
+				{
+					name: 'Tumblr',
+					link: 'https://www.tumblr.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
